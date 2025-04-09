3ae4b516b518d48c15e83f17368c16b26b3fd291
AlisherXursanov <alisherxursan@gmail.com>
2025-04-09 19:56:12 +0500

    Create hw1.sql

---
+++ lesson-1/homework/hw1.sql
@@ -0,0 +1,14 @@
+create database lesson1
+
+
+-- 1. Define the following terms: data, database, relational database, and table.
+
+jdqhuhoi
+qdkjhkjdhqk
+
+-- 2. List five key features of SQL Server.
+QHBDJQHWBHKJ
+S1KJQHWKSHK
+
+-- 3. 
+
